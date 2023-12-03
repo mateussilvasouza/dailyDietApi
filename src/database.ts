7b012409993c617ef9d4571979dac2cc90c9bef0
mateussilvasouza <mateus.souza@vaifacil.com>
2023-12-03 16:19:18 -0300

    Alter database

--- src/database.ts
+++ src/database.ts
@@ -7,14 +7,7 @@ export const config: Knex.Config = {
     ? {
         filename: env.DATABASE_URL
       }
-    : {
-        connectionString: env.DATABASE_URL,
-        host: env.DB_HOST,
-        port: env.PORT,
-        user: env.DB_USER,
-        database: env.DB_NAME,
-        password: env.DB_PASSWORD
-      },
+    : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
